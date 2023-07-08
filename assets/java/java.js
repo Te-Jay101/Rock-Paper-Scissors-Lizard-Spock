8c7d9bf8459133af58590c43923bd1772335aa29
Te-Jay Pitman <Tejay.pitman455@gmail.com>
2023-07-08 14:21:17 +0000

    Started java for game dynamics.

---
+++ assets/java/java.js
@@ -0,0 +1,15 @@
+const startBattle = () {
+    let humanPlayerScore = 0;
+    let evilComputerScore = 0;
+    let moves = 0;
+
+    const beginFight = () {
+        const rockAction = document.querySelector('.rock');
+        const paperAction = document.querySelector('.paper');
+        const scissorAction = document.querySelector('.scissors');
+        const lizardAction = document.querySelector('.lizard');
+        const spockAction = document.querySelector('.spock');
+        const playerOptions = [rockAction,paperAction,scissorsAction,lizardAction,spockAction]
+    }
+
+}
