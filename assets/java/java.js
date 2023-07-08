393ca36e4501c604e1d27c13bf13138ccec4a00d
Te-Jay Pitman <Tejay.pitman455@gmail.com>
2023-07-08 14:31:04 +0000

    Created round to go code.

--- assets/java/java.js
+++ assets/java/java.js
@@ -12,7 +12,22 @@ const startBattle = () {
         const playerOptions = [rockAction,paperAction,scissorsAction,lizardAction,spockAction];
         const computerChoices = ['rock','paper','scissors','lizard','spock'];
 
-        
+        playerOptions.forEach(Option => {
+            Option.addEventListener('click', function(){
+                const movesleft = document.querySelector('.movesleft');
+                moves++;
+                movesleft.innerText = `Rounds to go: ${10 - moves}`;
+
+                const choiceNumber = Math.floor(Math.random()*5);
+                const computerChoice = computerChoices [choiceNumber];
+
+                victor(this.innerText,computerChoice);
+
+                if (moves == 10){
+                    gameOver(playerOptions,movesLeft);
+                }
+            })
+        })
     }
 
 }
